ca4a5f63919aaaf80202899eb914c70cbdc4a1cf
vivek padia <vivekpadia70@gmail.com>
2017-10-10 19:25:55 +0530

    setting up

---
+++ src/app/app.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { IonicModule } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { MyApp } from './app.component';
+
+let comp: MyApp;
+let fixture: ComponentFixture<MyApp>;
+
+describe('Component: Root Component', () => {
+
+    beforeEach(async(() => {
+
+        TestBed.configureTestingModule({
+
+            declarations: [MyApp],
+
+            providers: [
+              StatusBar,
+              SplashScreen
+            ],
+
+            imports: [
+                IonicModule.forRoot(MyApp)
+            ]
+
+        }).compileComponents();
+
+    }));
+
+    beforeEach(() => {
+
+        fixture = TestBed.createComponent(MyApp);
+        comp    = fixture.componentInstance;
+
+    });
+
+    afterEach(() => {
+        fixture.destroy();
+        comp = null;
+    });
+
+    
+});
